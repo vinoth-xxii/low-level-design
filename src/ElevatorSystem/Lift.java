f8f5a7430df89ab6acbb29b9d00f9bf9a58b987f
Vinoth Kumar V <vinothkumar02141@gmail.com>
2026-07-13 17:06:36 +0530

    little more changes

--- src/ElevatorSystem/Lift.java
+++ src/ElevatorSystem/Lift.java
@@ -1,20 +1,17 @@
 package ElevatorSystem;
 
 public abstract  class Lift {
-    int capacity;
     int direction;
     int current_floor;
     Set<Integer> stopsList;
 
 
     public void goUp(int floor){
-
+        current_floor++;
     }
 
     public void goDown(int floor){
-
+        current_floor--;
     }
-
-    public abstract int getCapacity();
 
 }
